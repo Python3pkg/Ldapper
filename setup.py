e74f32cb7dc4d99cac8a795c2cdb747744305917
Johan Jatko <armedguy@xr.gs>
2016-10-30 15:30:19 +0100

    fixed tarball url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Johan Jatko',
   author_email = 'armedguy@ludd.ltu.se',
   url = 'https://github.com/ArmedGuy/Ldapper', # use the URL to the github repo
-  download_url = 'https://github.com/ArmedGuy/Ldapper/tarball/0.1.0', # I'll explain this in a second
+  download_url = 'https://github.com/ArmedGuy/Ldapper/tarball/0.1.1', # I'll explain this in a second
   keywords = ['ldap3','model'], # arbitrary keywords
   classifiers = []
 )
