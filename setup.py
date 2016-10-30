6d32d3bd3e0a448cef76b4279a4de0ca72e84506
Johan Jatko <armedguy@xr.gs>
2016-10-30 15:27:45 +0100

    make module pypi compatible

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ldapper',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'Model Library for ldap3',
+  author = 'Johan Jatko',
+  author_email = 'armedguy@ludd.ltu.se',
+  url = 'https://github.com/ArmedGuy/Ldapper', # use the URL to the github repo
+  download_url = 'https://github.com/ArmedGuy/Ldapper/tarball/0.1.0', # I'll explain this in a second
+  keywords = ['ldap3','model'], # arbitrary keywords
+  classifiers = []
+)
